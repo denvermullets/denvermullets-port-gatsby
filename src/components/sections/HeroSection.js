4d7b8053c3b5e8dee2fd93f3e41ffdf3a0ae222c
denvermullets <devmullets@gmail.com>
2020-11-25 10:58:50 -0500

    :sparkles: add basic styling and grid options, colorstyles initialized

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,13 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import { H1, MediumText } from "../styles/TextStyles"
 
 export default function HeroSection() {
   return (
-    <>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>Hi. I'm Ryan.</Title>
+          <Description>
+            I'm a software engineer, designer and photographer. I love working
+            with Javascript, Ruby, and React.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background: #292929;
+`
+// margin: top/bottom left/right
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`
+
+// css grid using gap instead of margins provides extra flexibility for mobile
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled(H1)`
+  color: white;
+`
+const Description = styled(MediumText)`
+  font-size: 17px;
+  line-height: 130%;
+`
---
+++ src/components/styles/ColorStyles.js
@@ -0,0 +1,5 @@
+export const themes = {
+  light: {
+    text1: "black",
+  },
+}
